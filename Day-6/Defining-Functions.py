16e09c51ff7b6529d6893f547b9d0aacbc7e9e1c
Aman Virani <viranipaman@gmail.com>
2025-04-06 14:06:59 +0530

    Day - 6 completed

---
+++ Day-6/Defining-Functions.py
@@ -0,0 +1,8 @@
+def myfunction():
+    print("AYEEEEEEEEE")
+
+
+myfunction()
+
+# You can define your own functions by using def before the function name and parentheses after it 
+# It can be used to make the code more efficient and reduce the amount of work done in writing the same code again and again
---
+++ Day-6/While_Loops.py
@@ -0,0 +1,9 @@
+aman =  10
+
+while aman > 0:
+    print("OLLA OLLAA LAAA MAI TERI FANTASYYYYY")
+    aman -= 1
+    print(aman)
+    
+    
+# While Loop is a type of a loop like for loop which works like while this is true or false this should happen
